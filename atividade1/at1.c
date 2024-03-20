4c8dffcd66578216606cbd1db6988484d0e4de65
Gustavo-CS <gustavocarvalho9340@gmail.com>
2024-03-20 05:59:53 -0300

    quick init

--- atividade1/at1.c
+++ atividade1/at1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void quicSort(int array3[]) {
+        if (sizeof(array3)/sizeof(array3[0]) < 2 ){
+            return array3;
+        } 
+        else {
+            int pivo = array3[0];
+            int menores[] = array3 <= pivo;
+            int maiores[] = array3 > pivo;
+            return quicSort(menores) + pivo + quicSort(maiores);
+        }
+    }
+
 int main(int argc, char *argv[])
 {
 
@@ -13,32 +25,33 @@ int main(int argc, char *argv[])
     int menor2 = 0;
     int array2[argc - 1];
 
-    // for (int i = 1; i < argc; i++)
-    // {
-    //     array2[i-1] = atoi(argv[i]);
-    // }
+    for (int i = 1; i < argc; i++)
+    {
+        array2[i-1] = atoi(argv[i]);
+    }
 
     // void ord() {
 
     // }
 
-    for (int i = 0; i < argc - 1; i++)
-    {
-        // array
-        // int tam = sizeof(array) / sizeof(array[0]);
+    
+    // for (int i = 0; i < argc - 1; i++)
+    // {
+    //     // array
+    //     // int tam = sizeof(array) / sizeof(array[0]);
 
-        valor = atoi(argv[i + 1]);
-        for (int j = 0; j < argc - 1; j++)
-        {
+    //     valor = atoi(argv[i + 1]);
+    //     for (int j = 0; j < argc - 1; j++)
+    //     {
 
-            if (valor > atoi(argv[j+1]))
-            {
-                menor2+=1;
-            }
-        }
-        // array2[indice] = -1;
-        array[menor2] = valor;
-    }
+    //         if (valor > atoi(argv[j+1]))
+    //         {
+    //             menor2+=1;
+    //         }
+    //     }
+    //     // array2[indice] = -1;
+    //     array[menor2] = valor;
+    // }
 
     for (int i = 1; i < argc; i++)
     {
