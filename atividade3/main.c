f6a9973b08586640525d4d3d1d090aa9d4b37e27
GUSTAVO CARVALHO SANTOS <gustavo.santos2023@a.ucb.br>
2024-04-09 11:18:09 -0300

    subindo

--- atividade3/main.c
+++ atividade3/main.c
@@ -1,40 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void swap(int *p1, int *p2)
-{
-    int temp;
-    temp = *p1;
-    *p1 = *p2;
-    *p2 = temp;
-}
-
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
+#include "bubble_sort.h"
 
 int main(int argc, char *argv[])
 {
@@ -48,8 +14,9 @@ int main(int argc, char *argv[])
     }
 
     int *ptr;
-    int size;
+    int size = 0;
     int txt;
+
     while (fscanf(arq, "%d", &txt) != EOF)
     {
         size++;
@@ -74,7 +41,26 @@ int main(int argc, char *argv[])
         }
         fclose(arq);
 
-        quickSort(ptr, 0, size - 1);
+        bubble_sort(ptr, size);
+
+        int valor2;
+        int valor;
+
+        for (int i = 0; i < size; i++)
+        {
+            valor = ptr[i];
+            valor2 = ptr[i];
+            for (int j = i; j < size; j++)
+            {
+                if (valor > ptr[j])
+                {
+                    valor2 = ptr[j];
+                    ptr[j] = valor;
+                    ptr[i] = valor2;
+                    valor = valor2;
+                }
+            }
+        }
 
         arq2 = fopen("saida.txt", "w");
 
