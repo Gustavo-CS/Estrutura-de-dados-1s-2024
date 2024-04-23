e1a65118ed9f1e7d6ff8177391b3f9428e85eda8
GUSTAVO CARVALHO SANTOS <gustavo.santos2023@a.ucb.br>
2024-04-23 10:40:44 -0300

    at4

---
+++ atividade4sala/linked_list.h
@@ -0,0 +1,23 @@
+typedef struct No 
+{
+    char valor;
+    struct No* proximo_no;
+    
+} No;
+
+
+No* no(char valor, No* proximo_no);
+
+void inserir_no(No* H, No* no);
+
+void imprimir_list(No* H);
+
+int quantidade_nos(No* H);
+
+No* copiar_list(No* H);
+
+void liberar_lista(No* H);
+
+int lista_verificar_existencia(No* H, char valor_busca);
+
+void lista_inserir_no_i(No* H, No* no, int i);
---
+++ atividade4sala/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+int main(int argc,char *argv[]) {
+    No* n1 = no('A', NULL);
+    No* n2 = no('B', NULL);
+    No* n3 = no('C', NULL);
+    No* n4 = no('D', NULL);
+
+    n1->proximo_no = n2;
+    n2->proximo_no = n3;
+
+    No* h = n1;
+    // No* h;
+
+    // inserir_no(h, n4);
+
+    lista_inserir_no_i(h, n4, 1);
+
+    imprimir_list(h);
+    
+    // No* h2 = copiar_list(h);
+
+    // imprimir_list(h);
+
+    // liberar_lista(h2);
+
+    // printf("\nqnt = %d \n", quantidade_nos(h));
+    // printf("\nqnt = %d \n", quantidade_nos(h2));
+
+    // printf("%d",lista_verificar_existencia(h, 'B'));
+    // if(lista_verificar_existencia(h, 'B') == 0){
+    //     printf("B nao esta na lista");
+    // }
+    // else {
+    //     printf("B esta na lista");
+
+    // }
+
+    
+    // do
+    // {
+    //     printf("%c \t", h->valor);
+    //     h = h->proximo_no;
+    // } while (h != NULL);
+    
+    
+    // printf("n1 = %c \t n2 = %c", n1->valor, n2->valor);
+
+    exit(0);
+}
